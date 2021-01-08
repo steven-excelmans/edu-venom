9adf79ac30e22ba961ed3137f511a15ccf480319
Steven Excelmans <steven.excelmans@telenet.be>
2021-01-08 15:18:20 +0100

    [VENoM] Vue boilerplate components

---
+++ client/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>About</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <Transactions></Transactions>
+    <EditTransactions></EditTransactions>
+  </div>
+</template>
+
+<script>
+import Transactions from '@/components/Transactions.vue'
+import EditTransactions from '@/components/EditTransactions'
+
+export default {
+  components: {
+    EditTransactions,
+    Transactions
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
